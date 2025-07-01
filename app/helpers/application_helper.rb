3671e185cf8f7f090e9a24f559b165b8e5e9e285
Sidra <alice@alum.mit.edu>
2025-07-01 15:49:37 -0400

    final rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
+# available in every view
 module ApplicationHelper
 end
--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ require 'action_view/railtie'
 Bundler.require(*Rails.groups)
 
 module Test
+  # the main Rails App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
