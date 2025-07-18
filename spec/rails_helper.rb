532920e900588beb7fc0e26dcb5da58f72eb785c
Sidra <alice@alum.mit.edu>
2025-07-18 08:53:05 -0400

    setup rspec

---
+++ spec/rails_helper.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+# Prevent running if the environment is production
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+
+require 'rspec/rails'
+# Add additional requires below this line. Rails is not loaded until this point!
+require Rails.root.join('features/support/defaults').to_s
+
+# Ensures that the test database schema matches the current schema file.
+# If there are pending migrations it will invoke `db:test:prepare` to
+# recreate the test database by loading the schema.
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before do
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.before do
+    DatabaseCleaner.start
+  end
+  config.after do
+    DatabaseCleaner.clean
+  end
+
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+  # arbitrary gems may also be filtered via:
+  # config.filter_gems_from_backtrace("gem name")
+end
